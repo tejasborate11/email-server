08f93b72482e6525578c41546b400ebd5f6fa796
= <=>
2024-03-13 00:32:40 +0530

    index.js

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const server = new SMTPServer({
   },
 });
 
-server.listen(25, () => {
-  console.log("Server listening on port 25");
+server.listen(465, () => {
+  console.log("Server listening on port 465");
 });
 
 // server.listen(465,() => {
