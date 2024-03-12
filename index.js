657050fb1115f354307b140bf2fac7d9a556011b
= <=>
2024-03-13 00:28:33 +0530

    index.js

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const SMTPServer = require("smtp-server").SMTPServer;
 
 const server = new SMTPServer({
-    allowInsecureAuth:true,
-    authOptional:true,
-    onConnect(session,cb){
-        console.log("onConnect==> ",session.id);
-        cb()
-    },
-    onMailFrom(address,session,cb){
-        console.log("onMailFrom==> ",address.address,session.id);
-        cb()
-    },
-    onRcptTo(address,session,cb){
-      console.log("onRcptTo==> ",address.address,session.id)
-      cb()
-    },
-    onData(stream,session,cb){
-        stream.on("data",(data)=>{
-            console.log("data===> ", `${data.toString()}`)
-        })
-        stream.on("end",cb)
-    }
+  allowInsecureAuth: true,
+  authOptional: true,
+  secure: true,
+  onConnect(session, cb) {
+    console.log("onConnect==> ", session.id);
+    cb();
+  },
+  onMailFrom(address, session, cb) {
+    console.log("onMailFrom==> ", address.address, session.id);
+    cb();
+  },
+  onRcptTo(address, session, cb) {
+    console.log("onRcptTo==> ", address.address, session.id);
+    cb();
+  },
+  onData(stream, session, cb) {
+    stream.on("data", (data) => {
+      console.log("data===> ", `${data.toString()}`);
+    });
+    stream.on("end", cb);
+  },
 });
 
-server.listen(25,() => {
-    console.log("Server listening on port 25")
+server.listen(25, () => {
+  console.log("Server listening on port 25");
 });
